45a42d065bea8df7dcf404fb8600cac37188f565
onodera_sf <sf_yo_service_yellow@outlook.jp>
2021-03-22 11:10:24 +0900

    プロジェクト ファイルを追加します。

---
+++ AppSettings/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AppSettings.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace AppSettings.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IConfiguration _configuration;
+
+		// DI で IConfiguration を受け取る
+		public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+		{
+			_logger = logger;
+			_configuration = configuration;
+		}
+
+		public IActionResult Index()
+		{
+			// appsettings.json から値を取得します。
+			// 階層化されている場合は GetSection を使用します。
+			ViewData["Message"] = _configuration.GetSection("TestObject").GetValue<string>("TestValue");
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
